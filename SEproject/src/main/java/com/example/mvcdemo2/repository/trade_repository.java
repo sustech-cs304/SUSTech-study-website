3badeb0dc3a79ad6d6fb34e9ebf51845f1b03f2a
lycnkd <12112723@mail.sustech.edu.cn>
2024-04-12 22:10:02 +0800

    update goods entity class

--- SEproject/src/main/java/com/example/mvcdemo2/repository/trade_repository.java
+++ SEproject/src/main/java/com/example/mvcdemo2/repository/trade_repository.java
@@ -1,6 +1,5 @@
 package com.example.mvcdemo2.repository;
 
-import com.example.mvcdemo2.model.goods;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
